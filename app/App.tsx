0a5fc726f26e7813a3ec67f4bc05195d44e13af9
Kian2002 <canadakurdistan@gmail.com>
2023-03-20 17:33:28 -0700

    Initial commit

---
+++ app/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+const App = () => {
+  return (
+    <>
+      <View className="flex-1 items-center justify-center bg-red-300"></View>
+    </>
+  );
+};
+
+export default App;
