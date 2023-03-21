b7b215bdde02e22aefecf5b5025ae4206bea2f78
Kian2002 <canadakurdistan@gmail.com>
2023-03-20 20:43:47 -0700

    setup app.tsx

--- app/App.tsx
+++ app/App.tsx
@@ -2,11 +2,42 @@ import { useState } from "react";
 import { View, ScrollView, SafeAreaView } from "react-native";
 import { Stack, useRouter } from "expo-router";
 
+import {
+  Nearbyjobs,
+  Popularjobs,
+  Welcome,
+  ScreenHeaderBtn,
+} from "../components";
+
+import { icons, images } from "../constants";
+
 const App = () => {
+  const router = useRouter();
+
   return (
-    <>
-      <View className="flex-1 items-center justify-center bg-red-300"></View>
-    </>
+    <SafeAreaView className="flex-1 bg-lightWhite">
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: "#FAFAFC" },
+          headerShadowVisible: false,
+          headerTitle: "",
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+        }}
+      />
+
+      <ScrollView showsHorizontalScrollIndicator={false}>
+        <View className="flex-1 p-medium">
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
