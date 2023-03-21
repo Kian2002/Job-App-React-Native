754cbc256fa9399d2e3152c24984875e88b4734e
Kian2002 <canadakurdistan@gmail.com>
2023-03-21 03:01:00 -0700

    set up .env, api, and popularjobs

--- app/app.d.ts
+++ app/app.d.ts
@@ -4,3 +4,7 @@ declare module "*.png";
 declare module "*.svg";
 declare module "*.jpeg";
 declare module "*.jpg";
+
+declare module '@env' {
+    export const RAPID_API_KEY: string;
+  }
--- components/home/popular/Popularjobs.tsx
+++ components/home/popular/Popularjobs.tsx
@@ -1,10 +1,43 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ActivityIndicator,
+  FlatList,
+} from "react-native";
+import useFetch from "../../../hooks/useFetch";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
 
 const Popularjobs = () => {
+  const query = { query: "react", num_pages: 1 };
+  const { isLoading, error, data } = useFetch("search", query);
+
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View className="w-full h-full mt-10">
+      <View className="flex-row justify-between items-center">
+        <Text className="font-med text-lg text-secondary">Popular Jobs</Text>
+        <TouchableOpacity>
+          <Text className="font-reg text-md text-gray">Show all</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        {isLoading ? (
+          <ActivityIndicator size="large" color="#0000ff" />
+        ) : error ? (
+          <Text>Something went wrong...</Text>
+        ) : (
+          <FlatList
+            data={data}
+            renderItem={({ item }) => <PopularJobCard />}
+            keyExtractor={(item) => item.id}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{ columnGap: 12 }}
+          />
+        )}
+      </View>
     </View>
   );
 };
