a26e66ac7aae4230aaa5b15b09cb8a7f8fa2f2df
Kian2002 <canadakurdistan@gmail.com>
2023-03-24 00:35:29 -0700

    helper function

---
+++ utils/checkImageUrl.ts
@@ -0,0 +1,9 @@
+const checkImageUrl = (url: string) => {
+    if (!url) return false
+    else {
+        const pattern = new RegExp('^https?:\\/\\/.+\\.(png|jpg|jpeg|bmp|gif|webp)$', 'i');
+        return pattern.test(url);
+    }
+}
+
+export default checkImageUrl;
