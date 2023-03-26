e412df21fe9326fc02b98e35dcdfdf21009d8726
Kian2002 <canadakurdistan@gmail.com>
2023-03-25 20:19:26 -0700

    remove uneeded code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,5 @@
 import { Stack } from "expo-router";
-import { useCallback } from "react";
 import { useFonts } from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
-
-SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
   const [fontsLoaded] = useFonts({
@@ -11,12 +7,6 @@ const Layout = () => {
     DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
     DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
   });
-
-  useCallback(async () => {
-    if (fontsLoaded) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
 
   if (!fontsLoaded) {
     return null;
