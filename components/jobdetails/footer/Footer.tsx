114f6ac25a7e8df63a35e797e78a93a1af73cc6b
Kian2002 <canadakurdistan@gmail.com>
2023-03-26 20:22:04 -0700

    create footer

--- components/jobdetails/footer/Footer.tsx
+++ components/jobdetails/footer/Footer.tsx
@@ -1,10 +1,31 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { FC } from "react";
+import { View, Text, TouchableOpacity, Image, Linking } from "react-native";
+import { icons } from "../../../constants";
 
-const Footer = () => {
+type FooterProps = {
+  url: string;
+};
+
+const Footer: FC<FooterProps> = ({ url }) => {
   return (
-    <View>
-      <Text>Footer</Text>
+    <View className="flex-row p-small justify-between items-center absolute bottom-0 left-0 right-0 bg-[#FFF]">
+      <TouchableOpacity className="border border-tertiary rounded-3xl h-16 w-16 items-center justify-center">
+        <Image
+          source={icons.heartOutline}
+          className=" h-2/5 w-2/5"
+          style={{ tintColor: "#FF7754" }}
+          resizeMode="contain"
+        />
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        className="bg-tertiary p-large flex-1 rounded-3xl ml-3"
+        onPress={() => Linking.openURL(url)}
+      >
+        <Text className="text-white font-dark text-md text-center">
+          Apply Now
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
